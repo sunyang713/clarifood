365b2db8ec95ab0be5e8d01b0a8035da0e81dbdf
Stephanie Huang <syh2115@columbia.edu>
2015-09-26 15:28:04 -0400

    add basic flask app structure

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Helloooooo World!"
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
