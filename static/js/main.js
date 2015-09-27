fd6d5260c43902655cf05024d699637ab40bc46e
Jonathan <sunyang713@hotmail.com>
2015-09-27 10:15:48 -0400

    Improves processing dots

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,19 @@
 $().ready(function(){
   $('.btn').click(function(event) {
+    p = "Processing"
+    $(event.currentTarget).html(p);
     setInterval(function() {
-      p = "Processing"
       setTimeout(function() {
         $(event.currentTarget).html(p + '.');
       }, 250);
       setTimeout(function() {
-        $(event.currentTarget).html(p + '.');
+        $(event.currentTarget).html(p + '..');
       }, 500);
       setTimeout(function() {
-        $(event.currentTarget).html(p + '.');
+        $(event.currentTarget).html(p + '...');
       }, 750);
       setTimeout(function() {
-        $(event.currentTarget).html(p + '.');
+        $(event.currentTarget).html(p + '....');
       }, 1000);
     }, 1000);
   });
