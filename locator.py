644f4dc37ec4c3031c46f06ef0f92e7d6e305070
Jonathan <sunyang713@hotmail.com>
2015-09-27 12:29:25 -0400

    Api

--- locator.py
+++ locator.py
@@ -1,6 +1,6 @@
 from googleplaces import GooglePlaces, types
 
-API_KEY = 'AIzaSyDUizBgsqedfEZI3_1hC2m3kHCFc50vcoo'
+API_KEY = 'AIzaSyANsWQQSxjFEGLoJdkuLu5AKExKC2ojsk4'
 
 google_places = GooglePlaces(API_KEY)
 
