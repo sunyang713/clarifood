3cf19e22da40d119024470aa5e882b15a4b817bb
Stephanie Huang <syh2115@columbia.edu>
2015-09-26 22:16:15 -0400

    add locator to return nearby vendors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import predictor
+import locator
 
 app = Flask(__name__)
 
@@ -11,12 +12,16 @@ def main():
 def about():
     return render_template('about.html')
 
-if __name__ == "__main__":
-    app.run()
-
 @app.route("/predict/<path:url>")
 def predict(url):
     return str(predictor.predict(url))
+
+@app.route("/locations/<path:url>")
+def locations(url):
+    if (predictor.predict(url)):
+       return str(locator.find(query="bubbletea"))
+    else:
+        return "Not bubble tea"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
