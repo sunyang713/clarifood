9dd9ebca3ac49c56c2700d28a9c7edd5a5a5eecc
Jonathan <sunyang713@hotmail.com>
2015-09-26 23:58:29 -0400

    Url submission

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 import predictor
 import locator
+import re
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-<<<<<<< HEAD
     return render_template('index.html', active='home')
 
 @app.route("/about")
@@ -14,16 +15,19 @@ def about():
     return render_template('about.html', active='about')
 
 
-@app.route('/post', methods=['GET', 'POST'])
-def post():
+@app.route('/find', methods=['GET', 'POST'])
+def find():
     if request.method == 'POST':
-		return predict(request.form['image_url'])
-
+        imageUrl = request.form['image_url']
+        isValidUrl = re.match('^https?://(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:/[^/#?]+)+\.(?:jpg|gif|png)$', imageUrl)
+        if isValidUrl:
+            return locations(request.form['image_url'])
+        else:
+            return render_template('index.html', active='home', error=True)
 
 @app.route("/predict/<path:url>")
 def predict(url):
     return str(predictor.predict(url))
-
 
 @app.route("/locations/<path:url>")
 def locations(url):
