b4982b8ebb8d24bfbc353263cd19d50a80695e14
Stephanie Huang <syh2115@columbia.edu>
2015-09-30 22:14:32 -0400

    add info box to location map

--- static/js/map.js
+++ static/js/map.js
@@ -126,6 +126,7 @@ var MAP_ICON = {
 };
 
 var map;
+var infoBox;
 
 function map() {
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
@@ -140,14 +141,48 @@ function map() {
         icon: MAP_ICON,
         position: {lat: loc.location.lat, lng: loc.location.lng}
       });
+      // attach event listener to marker
+      attachInfo(marker, loc);
       // extend bounds of map view
       bounds.extend(new google.maps.LatLng(loc.location.lat, loc.location.lng));
     }
   }
 }
 
+/*
+ * Attaches info box and event handler for when a marker is clicked.
+ * 'marker' is a google.maps.Marker object
+ * 'place' is an object containing info on the place
+ */
+function attachInfo(marker, place) {
+  // Add info box
+  var contentString = '';
+  if (place) {
+    contentString = '<div class="infobox">' + 
+      '<b>' + place.name + '</b><br>' +
+      place.address + '</div>';
+      addMarkListener(marker, contentString);
+  }
+}
+
+/*
+ * Adds a listener to a given marker
+ */
+function addMarkListener(marker, content) {
+  marker.info = content;
+
+  google.maps.event.addListener(marker, 'click', function () {
+    // Info box appears when marker clicked
+    infoBox.setContent(marker.info);
+    infoBox.open(map, marker);
+  });
+}
+
+
 function initialize() {
   map();
+
+  infoBox = new google.maps.InfoWindow({ maxWidth: 230 });
 }
 
 // load after page has loaded
