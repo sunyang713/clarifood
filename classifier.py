1d2eca6f9f4fadd6e06ff952802b875a6175ee9c
Jonathan <sunyang713@hotmail.com>
2015-09-30 21:00:48 -0400

    Adds recongized foods.

--- classifier.py
+++ classifier.py
@@ -3,7 +3,7 @@
 from clarifai_basic import ClarifaiCustomModel
 import json
 
-with open('tiny_categories.json') as json_file:
+with open('demo_categories.json') as json_file:
     data = json.load(json_file)
 
 def classify(url):
